70da28ea09e27c13d145ad298647210c5e17df66
Sabino <sabinoingeniero@gmail.com>
2022-11-17 12:42:19 -0600

    Bosquejo de la aplicacion

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,15 +4,18 @@ import { RouterModule } from '@angular/router';
 
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 
 @NgModule({
   declarations: [
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    ScrollToTopComponent
   ],
   exports:[
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    ScrollToTopComponent
   ],
   imports: [
     CommonModule,
